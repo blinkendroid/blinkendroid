5d8cf85cf1eaca503e5dfee5b921a867020a46b3
benjamin.r.m.weiss <benjamin.r.m.weiss@ded441ff-28c8-dbd9-35dd-4b5d9abe79e0>
2010-05-02 12:24:05 +0000

    First implementation of vibration.
    git-svn-id: https://blinkendroid.googlecode.com/svn/trunk@4 ded441ff-28c8-dbd9-35dd-4b5d9abe79e0

--- blinkendroid/src/org/cbase/blinkendroid/Blinkendroid.java
+++ blinkendroid/src/org/cbase/blinkendroid/Blinkendroid.java
@@ -2,6 +2,10 @@ package org.cbase.blinkendroid;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.os.Vibrator;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class Blinkendroid extends Activity {
     /** Called when the activity is first created. */
@@ -9,5 +13,21 @@ public class Blinkendroid extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        //Get the button and set the listener
+        Button b = (Button) this.findViewById(R.id.vibrate);
+        b.setOnClickListener(new OnClickListener() {
+	    
+	    @Override
+	    public void onClick(View v) {
+		vibrate();
+	    }
+	});
+    }
+    /**
+     * Vibrates the device.
+     */
+    public void vibrate() {
+	Vibrator vibrator = (Vibrator) getSystemService(VIBRATOR_SERVICE);
+	vibrator.vibrate(2000l);
     }
 }
