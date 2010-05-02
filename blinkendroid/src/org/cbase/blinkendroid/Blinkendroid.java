39d217926976c06da7f2fae814b4f1ab622a2623
benjamin.r.m.weiss <benjamin.r.m.weiss@ded441ff-28c8-dbd9-35dd-4b5d9abe79e0>
2010-05-02 12:06:40 +0000

    initial scaffold
    git-svn-id: https://blinkendroid.googlecode.com/svn/trunk@3 ded441ff-28c8-dbd9-35dd-4b5d9abe79e0

---
+++ blinkendroid/src/org/cbase/blinkendroid/Blinkendroid.java
@@ -0,0 +1,13 @@
+package org.cbase.blinkendroid;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Blinkendroid extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
