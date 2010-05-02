7215ebc5b45fad3fa179ff5934500eb2aa215cd3
benjamin.r.m.weiss <benjamin.r.m.weiss@ded441ff-28c8-dbd9-35dd-4b5d9abe79e0>
2010-05-02 20:15:30 +0000

    introduced a new view. all our old visual output is now located in debug.xml main.xml now is a switch for other views
    git-svn-id: https://blinkendroid.googlecode.com/svn/trunk@25 ded441ff-28c8-dbd9-35dd-4b5d9abe79e0

--- blinkendroid/src/org/cbase/blinkendroid/Blinkendroid.java
+++ blinkendroid/src/org/cbase/blinkendroid/Blinkendroid.java
@@ -21,218 +21,238 @@ import android.widget.Toast;
 
 public class Blinkendroid extends Activity {
 
-	public static final String LOG_TAG	=	"blinkendroids";
-	private VibrationListener vibrationListener;
-	/*
-	 * The views
-	 */
-	private TextView sensorTextView;
-	private TextView counterTextView;
-	/*
-	 * The buttons
-	 */
-	private Button vibrateButton;
-	private Button exitButton;
-	private Button calibrateButton;
-	private Button serverButton;
-	/*
-	 * Our Vibrator
-	 */
-	private Vibrator vibrator;
-	/**
-	 * Tells whether the vibrateButton has been clicked
-	 */
-	private boolean vibrateButtonClicked = false;
+    public static final String LOG_TAG = "blinkendroids";
+    private VibrationListener vibrationListener;
+    /*
+     * The views
+     */
+    private TextView sensorTextView;
+    private TextView counterTextView;
+    /*
+     * The buttons
+     */
+    private Button vibrateButton;
+    private Button exitButton;
+    private Button calibrateButton;
+    private Button serverButton;
+    private Button switchToMainButton;
+    /*
+     * Our Vibrator
+     */
+    private Vibrator vibrator;
+    /**
+     * Tells whether the vibrateButton has been clicked
+     */
+    private boolean vibrateButtonClicked = false;
 
-	private WakeLock wakeLock;
+    private WakeLock wakeLock;
 
-	private Server server;
-	private Handler handler;
-	
-	/** Called when the activity is first created. */
-	@Override
-	public void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.main);
-		// Initialize
-		wakeLock = ((PowerManager) getSystemService(POWER_SERVICE))
-				.newWakeLock(PowerManager.SCREEN_BRIGHT_WAKE_LOCK,
-						getString(R.string.app_name));
-		wakeLock.acquire();
-		calibrateButton = (Button) this.findViewById(R.id.calibrateButton);
-		calibrateButton.setOnClickListener(new OnClickListener() {
-			public void onClick(View v) {
-				Toast.makeText(getApplicationContext(),
-						getString(R.string.calibrating), Toast.LENGTH_SHORT)
-						.show();
-				String imei = ((TelephonyManager) getSystemService(TELEPHONY_SERVICE))
-						.getDeviceId();
-				Toast
-						.makeText(getApplicationContext(), imei,
-								Toast.LENGTH_LONG).show();
-				Log.i(this.getClass().getName(), "Calibrate Button pressed");
-			}
-		});
+    private Server server;
+    private Handler handler;
 
-		exitButton = (Button) this.findViewById(R.id.exitButton);
-		exitButton.setOnClickListener(new OnClickListener() {
-			public void onClick(View v) {
-				releaseWakeLock();
-				Log.i(this.getClass().getName(), "Exit Button pressed");
-				System.exit(0);
-			}
-		});
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+	super.onCreate(savedInstanceState);
+	setContentView(R.layout.main);
+	// Initialize
+	wakeLock = ((PowerManager) getSystemService(POWER_SERVICE))
+		.newWakeLock(PowerManager.SCREEN_BRIGHT_WAKE_LOCK,
+			getString(R.string.app_name));
+	wakeLock.acquire();
 
-		vibrateButton = (Button) this.findViewById(R.id.vibrateButton);
-		vibrateButton.setOnClickListener(new OnClickListener() {
-			public void onClick(View v) {
-				vibrate();
-				Log.i(this.getClass().getName(), "Vibrate Button pressed");
-				setButtonClicked(true);
-			}
-		});
+	switchToMainButton = (Button) this
+		.findViewById(R.id.switchToMainButton);
+	switchToMainButton.setOnClickListener(new OnClickListener() {
 
-		server	=	new Server(this);		
-		serverButton = (Button) this.findViewById(R.id.ServerButton);
-		serverButton.setOnClickListener(new OnClickListener() {
-			public void onClick(View v) {
-				if(server.running){
-					server.end();
-					serverButton.setText(R.string.serverbuttonstart);
-				}else{
-					server.start();
-					serverButton.setText(R.string.serverbuttonstop);}
-			}
-		});
-		
-		counterTextView = (TextView) this.findViewById(R.id.CounterTextView);
-		sensorTextView = (TextView) this.findViewById(R.id.SensorsTextView);
-		// End initialize
+	    @Override
+	    public void onClick(View v) {
+		switchToDebugView();
+	    }
+	});
 
-		getVibration();
+    }
+
+    @Override
+    protected void onDestroy() {
+	Log.i(this.getClass().getName(), "onDestroy()");
+	releaseWakeLock();
+	super.onDestroy();
+    }
+
+    @Override
+    protected void onPause() {
+	Log.i(this.getClass().getName(), "onPause()");
+	releaseWakeLock();
+	super.onPause();
+    }
+
+    @Override
+    protected void onStop() {
+	releaseWakeLock();
+	Log.i(this.getClass().getName(), "onStop()");
+	super.onStop();
+    }
+
+    @Override
+    protected void onRestart() {
+	wakeLock.acquire();
+	Log.i(this.getClass().getName(), "onRestart()");
+	super.onRestart();
+    }
+
+    @Override
+    protected void onResume() {
+	wakeLock.acquire();
+	Log.i(this.getClass().getName(), "onResume()");
+	super.onResume();
+    }
+
+    /**
+     * Vibrates the device.
+     */
+    public void vibrate() {
+	SensorManager sm = (SensorManager) getSystemService(SENSOR_SERVICE);
+	sm.unregisterListener(getVibrationListener());
+	vibrator = (Vibrator) getSystemService(VIBRATOR_SERVICE);
+
+	TaskPool tp = new TaskPool(this);
+	Timer t = new Timer();
+	t.schedule(tp.createVibrationTask(500), 0);
+	t.schedule(tp.createVibrationTask(500), 1000);
+	t.schedule(tp.createVibrationTask(500), 2000);
+	t.schedule(tp.createReconnectTask(), 2600);
+	sensorTextView.setBackgroundColor(Color.RED);
+    }
+
+    /**
+     * Gets the vibration from another source.
+     */
+    public void getVibration() {
+	SensorManager sm = (SensorManager) getSystemService(SENSOR_SERVICE);
+	sm.registerListener(getVibrationListener(), sm
+		.getDefaultSensor(Sensor.TYPE_ACCELEROMETER),
+		SensorManager.SENSOR_DELAY_GAME);
+    }
+
+    /**
+     * Vibrates for a given time
+     * 
+     * @param durationMS
+     *            The time in milliseconds
+     */
+    public void vibrate(long durationMS) {
+	vibrator.vibrate(durationMS);
+    }
+
+    /**
+     * Gets a {@link VibrationListener}
+     * 
+     * @return see above
+     */
+    private VibrationListener getVibrationListener() {
+	if (vibrationListener == null) {
+	    vibrationListener = new VibrationListener(this);
 	}
+	return vibrationListener;
+    }
 
-	@Override
-	protected void onDestroy() {
-		Log.i(this.getClass().getName(), "onDestroy()");
+    /**
+     * @return the counterTextView
+     */
+    public TextView getCounterTextView() {
+	return counterTextView;
+    }
+
+    /**
+     * @return the sensorTextView
+     */
+    public TextView getSensorTextView() {
+	return sensorTextView;
+    }
+
+    /**
+     * @param buttonClicked
+     *            the buttonClicked to set
+     */
+    public void setButtonClicked(boolean buttonClicked) {
+	this.vibrateButtonClicked = buttonClicked;
+    }
+
+    /**
+     * @return the buttonClicked
+     */
+    public boolean isVibrateButtonClicked() {
+	return vibrateButtonClicked;
+    }
+
+    public Handler getHandler() {
+	if (null == handler)
+	    handler = new Handler();
+	return handler;
+    }
+
+    private void releaseWakeLock() {
+	if (wakeLock.isHeld())
+	    wakeLock.release();
+    }
+    
+    /**
+     * Initialization of objects for the debug view.
+     */
+    private void switchToDebugView() {
+	setContentView(R.layout.debug);
+	calibrateButton = (Button) this.findViewById(R.id.calibrateButton);
+	calibrateButton.setOnClickListener(new OnClickListener() {
+	    public void onClick(View v) {
+		Toast.makeText(getApplicationContext(),
+			getString(R.string.calibrating), Toast.LENGTH_SHORT)
+			.show();
+		String imei = ((TelephonyManager) getSystemService(TELEPHONY_SERVICE))
+			.getDeviceId();
+		Toast
+			.makeText(getApplicationContext(), imei,
+				Toast.LENGTH_LONG).show();
+		Log.i(this.getClass().getName(), "Calibrate Button pressed");
+	    }
+	});
+
+	exitButton = (Button) this.findViewById(R.id.exitButton);
+	exitButton.setOnClickListener(new OnClickListener() {
+	    public void onClick(View v) {
 		releaseWakeLock();
-		super.onDestroy();
-	}
+		Log.i(this.getClass().getName(), "Exit Button pressed");
+		System.exit(0);
+	    }
+	});
 
-	@Override
-	protected void onPause() {
-		Log.i(this.getClass().getName(), "onPause()");
-		releaseWakeLock();
-		super.onPause();
-	}
+	vibrateButton = (Button) this.findViewById(R.id.vibrateButton);
+	vibrateButton.setOnClickListener(new OnClickListener() {
+	    public void onClick(View v) {
+		vibrate();
+		Log.i(this.getClass().getName(), "Vibrate Button pressed");
+		setButtonClicked(true);
+	    }
+	});
 
-	@Override
-	protected void onStop() {
-		releaseWakeLock();
-		Log.i(this.getClass().getName(), "onStop()");
-		super.onStop();
-	}
-
-	@Override
-	protected void onRestart() {
-		wakeLock.acquire();
-		Log.i(this.getClass().getName(), "onRestart()");
-		super.onRestart();
-	}
-
-	@Override
-	protected void onResume() {
-		wakeLock.acquire();
-		Log.i(this.getClass().getName(), "onResume()");
-		super.onResume();
-	}
-
-	/**
-	 * Vibrates the device.
-	 */
-	public void vibrate() {
-		SensorManager sm = (SensorManager) getSystemService(SENSOR_SERVICE);
-		sm.unregisterListener(getVibrationListener());
-		vibrator = (Vibrator) getSystemService(VIBRATOR_SERVICE);
-
-		TaskPool tp = new TaskPool(this);
-		Timer t = new Timer();
-		t.schedule(tp.createVibrationTask(500), 0);
-		t.schedule(tp.createVibrationTask(500), 1000);
-		t.schedule(tp.createVibrationTask(500), 2000);
-		t.schedule(tp.createReconnectTask(), 2600);
-		sensorTextView.setBackgroundColor(Color.RED);
-	}
-
-	/**
-	 * Gets the vibration from another source.
-	 */
-	public void getVibration() {
-		SensorManager sm = (SensorManager) getSystemService(SENSOR_SERVICE);
-		sm.registerListener(getVibrationListener(), sm
-				.getDefaultSensor(Sensor.TYPE_ACCELEROMETER),
-				SensorManager.SENSOR_DELAY_GAME);
-	}
-
-	/**
-	 * Vibrates for a given time
-	 * 
-	 * @param durationMS
-	 *            The time in milliseconds
-	 */
-	public void vibrate(long durationMS) {
-		vibrator.vibrate(durationMS);
-	}
-
-	/**
-	 * Gets a {@link VibrationListener}
-	 * 
-	 * @return see above
-	 */
-	private VibrationListener getVibrationListener() {
-		if (vibrationListener == null) {
-			vibrationListener = new VibrationListener(this);
+	server = new Server(this);
+	serverButton = (Button) this.findViewById(R.id.ServerButton);
+	serverButton.setOnClickListener(new OnClickListener() {
+	    public void onClick(View v) {
+		if (server.running) {
+		    server.end();
+		    serverButton.setText(R.string.serverbuttonstart);
+		} else {
+		    server.start();
+		    serverButton.setText(R.string.serverbuttonstop);
 		}
-		return vibrationListener;
-	}
+	    }
+	});
 
-	/**
-	 * @return the counterTextView
-	 */
-	public TextView getCounterTextView() {
-		return counterTextView;
-	}
+	counterTextView = (TextView) this.findViewById(R.id.CounterTextView);
+	sensorTextView = (TextView) this.findViewById(R.id.SensorsTextView);
+	// End initialize
 
-	/**
-	 * @return the sensorTextView
-	 */
-	public TextView getSensorTextView() {
-		return sensorTextView;
-	}
-
-	/**
-	 * @param buttonClicked
-	 *            the buttonClicked to set
-	 */
-	public void setButtonClicked(boolean buttonClicked) {
-		this.vibrateButtonClicked = buttonClicked;
-	}
-
-	/**
-	 * @return the buttonClicked
-	 */
-	public boolean isVibrateButtonClicked() {
-		return vibrateButtonClicked;
-	}
-
-	private void releaseWakeLock() {
-		if (wakeLock.isHeld())
-			wakeLock.release();
-	}
-
-	public Handler getHandler() {
-		if(null==handler)
-			handler= new Handler();
-		return handler;
-	}
+	getVibration();
+    }
 }
