2bc8ee097d5b1bd12bc8585aa0726ae3714821f6
benjamin.r.m.weiss <benjamin.r.m.weiss@ded441ff-28c8-dbd9-35dd-4b5d9abe79e0>
2010-05-03 15:38:48 +0000

    One more token command: "setMatrixSize a b" should be able to set the matrixsize to any value you want.
    git-svn-id: https://blinkendroid.googlecode.com/svn/trunk@35 ded441ff-28c8-dbd9-35dd-4b5d9abe79e0

--- blinkendroid/src/org/cbase/blinkendroid/ConnectionThread.java
+++ blinkendroid/src/org/cbase/blinkendroid/ConnectionThread.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
+import java.util.StringTokenizer;
 
 import android.util.Log;
 import android.widget.Toast;
@@ -44,14 +45,25 @@ public class ConnectionThread extends Thread {
 
 	private String processInput(String inputLine) {
 		new ToastPoster(blinkendroid, inputLine, Toast.LENGTH_SHORT);
+		//Here are the commands to send or get data from the device:
 		if (inputLine.equals("getImei")) {
 		    return blinkendroid.getImei();
+			//Further getter examples
 		} else if (inputLine.equals("getLocationX")) {
 		    return Integer.toString(blinkendroid.getLocationX());
 		} else if (inputLine.equals("getLocationY")) {
 		    return Integer.toString(blinkendroid.getLocationY()); 
 		} else if (inputLine.equals("getLocationInMatrix")) {
 		    return blinkendroid.getLocationX() + "/" + blinkendroid.getLocationY();
+		    
+		    //Example for a setter:
+		} else if (inputLine.startsWith("setMatrixSize")) {
+		    StringTokenizer tokenizer = new StringTokenizer(inputLine, " ");
+		    if (tokenizer.countTokens() == 3) {
+			return "Matrix size set to " + tokenizer.nextToken() + " " + tokenizer.nextToken();
+		    } else {
+			return "tokencount invalid";
+		    }
 		} else {
 		    return new StringBuffer(inputLine).reverse().toString();
 		}
