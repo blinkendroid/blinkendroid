d5d9a124200127fc8dc9cc753c70eedd25870fc7
andreas.schildbach <andreas.schildbach@ded441ff-28c8-dbd9-35dd-4b5d9abe79e0>
2010-05-16 17:33:52 +0000

    start server
    git-svn-id: https://blinkendroid.googlecode.com/svn/trunk@122 ded441ff-28c8-dbd9-35dd-4b5d9abe79e0

--- blinkendroid/src/org/cbase/blinkendroid/ServerActivity.java
+++ blinkendroid/src/org/cbase/blinkendroid/ServerActivity.java
@@ -1,9 +1,16 @@
 package org.cbase.blinkendroid;
 
+import org.cbase.blinkendroid.network.multicast.SenderThread;
+
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.KeyEvent;
+import android.widget.TextView;
+import android.widget.TextView.OnEditorActionListener;
 
 public class ServerActivity extends Activity {
+
+    private SenderThread senderThread;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -11,5 +18,27 @@ public class ServerActivity extends Activity {
 	super.onCreate(savedInstanceState);
 
 	setContentView(R.layout.server);
+
+	TextView serverNameView = (TextView) findViewById(R.id.server_name);
+	serverNameView.setOnEditorActionListener(new OnEditorActionListener() {
+
+	    public boolean onEditorAction(TextView v, int actionId,
+		    KeyEvent event) {
+		senderThread = new SenderThread(v.getText().toString());
+		senderThread.start();
+		return true;
+	    }
+	});
+    }
+
+    @Override
+    protected void onDestroy() {
+
+	if (senderThread != null) {
+	    senderThread.interrupt();
+	    senderThread = null;
+	}
+
+	super.onDestroy();
     }
 }
