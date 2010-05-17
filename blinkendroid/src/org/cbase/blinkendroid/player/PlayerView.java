8d857b79354d9037aab8a8762032ac609308652e
andreas.schildbach <andreas.schildbach@ded441ff-28c8-dbd9-35dd-4b5d9abe79e0>
2010-05-17 10:36:12 +0000

    code cleanup
    git-svn-id: https://blinkendroid.googlecode.com/svn/trunk@144 ded441ff-28c8-dbd9-35dd-4b5d9abe79e0

--- blinkendroid/src/org/cbase/blinkendroid/player/PlayerView.java
+++ blinkendroid/src/org/cbase/blinkendroid/player/PlayerView.java
@@ -17,8 +17,7 @@
 
 package org.cbase.blinkendroid.player;
 
-import org.cbase.blinkendroid.Player;
-
+import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
@@ -26,77 +25,52 @@ import android.view.View;
 
 public class PlayerView extends View {
 
-	private Paint paint;
-	private Paint paint2;
-	private int width;
-	private int height;
-	private int frame;
-	public long lastFrameShowed=0;
-	public long lastFrame=0;
-	private int matrix[][]=null;
-	
+    private final Paint paint = new Paint();
+    private int width;
+    private int height;
+    private int frame;
+    public long lastFrameShowed = 0;
+    public long lastFrame = 0;
+    private int matrix[][] = null;
 
-	public PlayerView(Player player) {
-		super(player);
-		paint = new Paint();
-		paint2 = new Paint();
-		paint.setColor(Color.WHITE);
-		paint2.setColor(Color.BLACK);
-		paint2.setTextSize(16);
+    public PlayerView(final Context context) {
+	super(context);
+	paint.setColor(Color.WHITE);
+    }
+
+    public void setMatrix(int[][] matrix, int width, int height, int frame) {
+	this.matrix = matrix;
+	this.width = width;
+	this.height = height;
+	this.frame = frame;
+	this.lastFrameShowed = 0;
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+
+	super.onDraw(canvas);
+
+	if (null == matrix)
+	    return;
+	int pwidth = (getWidth() - width * 3) / width;
+	int pheight = (getHeight() - height * 3) / height;
+
+	int y = 0;
+	for (int i = 0; i < height; i++) {
+	    // Log.i("PlayerView", "draw row "+i);
+	    int x = 0;
+	    for (int j = 0; j < width; j++) {
+		paint.setColor(Color.argb(255, matrix[i][j] * 16,
+			matrix[i][j] * 16, matrix[i][j] * 16));
+		canvas.drawRect(x, y, x + pwidth, y + pheight, paint);
+		x += pwidth + 3;
+	    }
+	    y += pheight + 3;
 	}
-
-	@Override
-	protected void onDraw(Canvas canvas) {
-		super.onDraw(canvas);
-//		canvas.drawColor(Color.BL);
-//		canvas.drawText(Long.toString(System.currentTimeMillis()), 100, 100, paint);
-//		if(width>height){
-//			getW
-//		}else{
-//			
-//		}
-		if(null==matrix)
-			return;
-		int pwidth=(getWidth()-width*3)/width;
-		int pheight=(getHeight()-height*3)/height;
-		
-		int y=0;
-		for (int i = 0; i < height; i++) {
-//			Log.i("PlayerView", "draw row "+i);
-			int x=0;
-			for (int j = 0; j < width; j++) {
-				paint.setColor(Color.argb(255, matrix[i][j]*16, matrix[i][j]*16, matrix[i][j]*16));
-				canvas.drawRect(x, y, x+pwidth, y+pheight, paint);
-				x+=pwidth+3;
-			}
-			y+=pheight+3;
-		}
-		if(0==lastFrameShowed){
-			lastFrameShowed=System.currentTimeMillis();
-			lastFrame=frame;
-		}
+	if (0 == lastFrameShowed) {
+	    lastFrameShowed = System.currentTimeMillis();
+	    lastFrame = frame;
 	}
-
-	public void setMatrix(int[][] matrix, int width, int height, int frame) {
-		this.matrix=matrix;
-		this.width=width;
-		this.height=height;
-		this.frame=frame;
-		this.lastFrameShowed=0;
-	}
-
-	/**
-	 * @see android.view.View#measure(int, int)
-	 */
-//	@Override
-//	protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
-//		// Log.d(Blinkendroid.LOG_TAG,
-//		// "onMeasure: "+MeasureSpec.getSize(widthMeasureSpec)+":"+MeasureSpec.getSize(heightMeasureSpec));
-//		// setMeasuredDimension(256,256);
-//		setMeasuredDimension(OldBlinkendroid.screenWidth - 20,
-//				OldBlinkendroid.screenHeight - 100/*
-//												 * MeasureSpec.getSize(heightMeasureSpec
-//												 * )
-//												 */);
-//	}
+    }
 }
