23f7cf4da74dce5b170f9d2332a70d109431f6fb
benjamin.r.m.weiss <benjamin.r.m.weiss@ded441ff-28c8-dbd9-35dd-4b5d9abe79e0>
2010-05-18 19:12:48 +0000

    comments & exception handling & merged
    git-svn-id: https://blinkendroid.googlecode.com/svn/trunk@189 ded441ff-28c8-dbd9-35dd-4b5d9abe79e0

--- blinkendroid/src/org/cbase/blinkendroid/network/BlinkendroidProtocol.java
+++ blinkendroid/src/org/cbase/blinkendroid/network/BlinkendroidProtocol.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
+import java.net.SocketException;
 import java.util.HashMap;
 
 import org.cbase.blinkendroid.Constants;
@@ -24,7 +25,7 @@ public class BlinkendroidProtocol {
     Socket socket;
     GlobalTimerThread globalTimerThread;
     RecieverThread recieverThread;
-    
+
     private final HashMap<String, ICommandHandler> handlers = new HashMap<String, ICommandHandler>();
 
     public BlinkendroidProtocol(Socket socket, boolean server) {
@@ -39,7 +40,7 @@ public class BlinkendroidProtocol {
 	}
 	this.server = server;
 	// if(!server)
-	recieverThread	=	new RecieverThread();
+	recieverThread = new RecieverThread();
 	recieverThread.start();
     }
 
@@ -51,7 +52,9 @@ public class BlinkendroidProtocol {
 	handlers.remove(handler);
     }
     public void startTimerThread() {
-	// if(null==globalTimerThread)
+	if (globalTimerThread != null) {
+	    globalTimerThread.shutdown();
+	}
 	globalTimerThread = new GlobalTimerThread();
 	globalTimerThread.start();
     }
@@ -61,6 +64,7 @@ public class BlinkendroidProtocol {
 	try {
 	    in.close();
 	    socket.close();
+	    Log.d(Constants.LOG_TAG, "BlinkendroidProtocol: Socket closed.");
 	} catch (IOException e) {
 	    // TODO Auto-generated catch block
 	    e.printStackTrace();
@@ -68,16 +72,29 @@ public class BlinkendroidProtocol {
 
     }
 
+    public void shutdown() {
+	if (null != globalTimerThread)
+	    globalTimerThread.shutdown();
+	recieverThread.shutdown();
+	Log.i(Constants.LOG_TAG, "Protocol shutdown.");
+	close();
+    }
+
+    // Inner classes:
+    /**
+     * A thread that receives information from a Blinkendroid server.
+     */
     private class RecieverThread extends Thread {
-	// private boolean running = true;
+	private boolean running = true;
 
 	@Override
 	public void run() {
+	    running = true;
 	    Log.i(Constants.LOG_TAG, "InputThread started "
 		    + (server ? "server" : "client"));
 	    String inputLine;
 	    try {
-		while ((inputLine = in.readLine()) != null) {
+		while (running && (inputLine = in.readLine()) != null) {
 		    Log.i(Constants.LOG_TAG, "InputThread recieved: "
 			    + inputLine);
 		    String proto = inputLine.substring(0, 1);
@@ -85,6 +102,8 @@ public class BlinkendroidProtocol {
 		    if (null != handler)
 			handler.handle(inputLine.substring(1).getBytes());
 		}
+	    }catch (SocketException e) {
+		Log.d(Constants.LOG_TAG, "Socket closed.");
 	    } catch (NumberFormatException e) {
 		Log.e(Constants.LOG_TAG, "InputThread fucked "
 			+ (server ? "server" : "client"), e);
@@ -95,12 +114,17 @@ public class BlinkendroidProtocol {
 	    Log.i(Constants.LOG_TAG, "InputThread ended!!!!!!! "
 		    + (server ? "server" : "client"));
 	}
-	
+
 	public void shutdown() {
+	    Log.d(Constants.LOG_TAG, "RecieverThread shutdown.");
+	    running = false;
 	    interrupt();
 	}
     }
 
+    /**
+     * This thread sends the global time to connected devices.
+     */
     private class GlobalTimerThread extends Thread {
 	private boolean running = true;
 
@@ -117,28 +141,21 @@ public class BlinkendroidProtocol {
 		out.write(PROTOCOL_PLAYER + COMMAND_PLAYER_TIME
 			+ Long.toString(t) + '\n');
 		out.flush();
-		
-		//testing clipping
-		if(t%3==0){
-			out.write(PROTOCOL_PLAYER + COMMAND_CLIP
-				+ "0,0,"+(t%20)+","+(t%20) + '\n');
-			out.flush();
+
+		// testing clipping
+		if (t % 3 == 0) {
+		    out.write(PROTOCOL_PLAYER + COMMAND_CLIP + "0,0,"
+			    + (t % 20) + "," + (t % 20) + '\n');
+		    out.flush();
 		}
-		    
+
 	    }
 	}
 
 	public void shutdown() {
 	    running = false;
+	    Log.d(Constants.LOG_TAG, "GlobalTimerThread shutdown.");
 	    interrupt();
 	}
     }
-
-    public void shutdown() {
-	if(null!=globalTimerThread)
-	    globalTimerThread.shutdown();
-	recieverThread.shutdown();
-	close();
-    }
-
 }
