1b490a178125411023448bc343720445f924c48f
andreas.schildbach <andreas.schildbach@ded441ff-28c8-dbd9-35dd-4b5d9abe79e0>
2010-05-22 17:55:48 +0000

    client list in server view
    git-svn-id: https://blinkendroid.googlecode.com/svn/trunk@305 ded441ff-28c8-dbd9-35dd-4b5d9abe79e0

--- blinkendroid/src/org/cbase/blinkendroid/ServerActivity.java
+++ blinkendroid/src/org/cbase/blinkendroid/ServerActivity.java
@@ -12,23 +12,26 @@ import org.cbase.blinkendroid.utils.NetworkUtils;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 import android.widget.AdapterView.OnItemSelectedListener;
 
-public class ServerActivity extends Activity implements ConnectionListener, BLMManagerListener {
+public class ServerActivity extends Activity implements ConnectionListener,
+	BLMManagerListener {
 
     private SenderThread senderThread;
     private BlinkendroidServer blinkendroidServer;
     private BLMManager blmManager;
-    ArrayAdapter<String> adapter;
+    private ArrayAdapter<String> movieAdapter;
+    private ArrayAdapter<String> clientAdapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
@@ -41,32 +44,39 @@ public class ServerActivity extends Activity implements ConnectionListener, BLMM
 	final Button startButton = (Button) findViewById(R.id.server_start);
 	final Button stopButton = (Button) findViewById(R.id.server_stop);
 	final Button clientButton = (Button) findViewById(R.id.server_client);
+	final ListView clientList = (ListView) findViewById(R.id.server_client_list);
 
-	adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item);
-	adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-	movieSpinner.setAdapter(adapter);
-	//add default video
-	adapter.add("Blinkendroid");
-	adapter.add("Random");
-	blmManager=new BLMManager();
-	//read videos from /sdcard/blinkendroid
+	movieAdapter = new ArrayAdapter<String>(this,
+		android.R.layout.simple_spinner_item);
+	movieAdapter
+		.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+	movieSpinner.setAdapter(movieAdapter);
+	// add default video
+	movieAdapter.add("Blinkendroid");
+	movieAdapter.add("Random");
+	blmManager = new BLMManager();
 	blmManager.readMovies(this);
+
+	clientAdapter = new ArrayAdapter<String>(this,
+		android.R.layout.simple_list_item_1);
+	clientList.setAdapter(clientAdapter);
 
 	movieSpinner.setOnItemSelectedListener(new OnItemSelectedListener() {
 
 	    public void onItemSelected(AdapterView<?> arg0, View arg1,
 		    int arg2, long arg3) {
-		//already running?
-		if(null!=blinkendroidServer && blinkendroidServer.isRunning()){
-		    blinkendroidServer.switchMovie(blmManager.getBLMHeader(arg2));
+		// already running?
+		if (null != blinkendroidServer
+			&& blinkendroidServer.isRunning()) {
+		    blinkendroidServer.switchMovie(blmManager
+			    .getBLMHeader(arg2));
 		}
 	    }
 
 	    public void onNothingSelected(AdapterView<?> arg0) {
-
 	    }
 	});
-	
+
 	startButton.setOnClickListener(new OnClickListener() {
 
 	    public void onClick(View v) {
@@ -132,35 +142,28 @@ public class ServerActivity extends Activity implements ConnectionListener, BLMM
 	super.onDestroy();
     }
 
-    public void connectionClosed(SocketAddress socketAddress) {
-	runOnUiThread(new ToastPost("removed " + socketAddress.toString(),
-		Toast.LENGTH_SHORT));
+    public void connectionOpened(final SocketAddress socketAddress) {
+	runOnUiThread(new Runnable() {
+	    public void run() {
+		clientAdapter.add(socketAddress.toString());
+	    }
+	});
     }
 
-    public void connectionOpened(SocketAddress socketAddress) {
-	runOnUiThread(new ToastPost("joined " + socketAddress.toString(),
-		Toast.LENGTH_SHORT));
-    }
-
-    private class ToastPost implements Runnable {
-	String toast;
-	int length;
-
-	public ToastPost(String toast, int length) {
-	    this.toast = toast;
-	    this.length = length;
-	}
-
-	public void run() {
-	    Toast.makeText(ServerActivity.this, toast, length).show();
-	}
+    public void connectionClosed(final SocketAddress socketAddress) {
+	runOnUiThread(new Runnable() {
+	    public void run() {
+		clientAdapter.remove(socketAddress.toString());
+	    }
+	});
     }
 
     public void moviesReady() {
 	runOnUiThread(new Runnable() {
 	    public void run() {
-		blmManager.fillArrayAdapter(adapter);
-		Toast.makeText(ServerActivity.this, "Movies ready", Toast.LENGTH_SHORT).show();
+		blmManager.fillArrayAdapter(movieAdapter);
+		Toast.makeText(ServerActivity.this, "Movies ready",
+			Toast.LENGTH_SHORT).show();
 	    }
 	});
     }
